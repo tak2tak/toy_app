4038cdaf37b8e78ff5bb9cfa2215c10e859c5e7b
tak2tak <into@bokunomad.com>
2018-07-06 12:53:15 +0000

    finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'user#index'
+
+  resources :users
+  resources :microposts
 end
